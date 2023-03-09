8c49fb5cbf5ba8ec3224a5439fe0074c4fb70610
CesarGomezPeloso <cesargomezpeloso@outlook.com>
2023-03-09 16:33:39 -0300

    De 0

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import './App.css';
-import ListaPersonas from "../src/components/ListaPersonas/ListaPersonas/ListaPersonas";
 
-const listaDePersonas = [
-  {name:"Cesar Gomez", edad:38},
-  {name:"Nelson Masias", edad:20},
-  {name:"Tomas Juntel", edad:40},
-  {name:"Jorge Vega", edad:31},
-  {name:"Viviana Galvano", edad:23},
-];
+
 
 function App() {
   
-  return(
-  
-  <div className ="App">  
-    <ListaPersonas lista={listaDePersonas} />
-  </div>
-  );  
+ 
 }
 
 export default App;
