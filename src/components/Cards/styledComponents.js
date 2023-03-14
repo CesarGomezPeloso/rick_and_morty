d5542ec068567015259cdc25235122c888d11250
CesarGomezPeloso <cesargomezpeloso@outlook.com>
2023-03-14 06:50:18 -0300

    Correccion backgroungColor

--- src/components/Cards/styledComponents.js
+++ src/components/Cards/styledComponents.js
@@ -6,7 +6,7 @@ export const CardsContainer = styled.div`
     
     display: flex;
     /* flex-direction: row; */
-    justify-content: space-between; */
+    justify-content: space-between; 
     width: 20%;
     /* margin: auto; */
     /* background: #155263; */ 
@@ -14,6 +14,6 @@ export const CardsContainer = styled.div`
     
     padding: 1em;
     /* border-radius: 15px; */
-    /* box-shadow: 10px 10px 5px 0px rgba(119,98,140,0.75);  */ */
+    /* box-shadow: 10px 10px 5px 0px rgba(119,98,140,0.75);  */ 
  
 `
