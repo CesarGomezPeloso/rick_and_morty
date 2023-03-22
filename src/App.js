86d194f94ff75fa3eb1c8373d912ba890ec8d8f5
CesarGomezPeloso <cesargomezpeloso@outlook.com>
2023-03-22 09:44:19 -0300

    React-Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Cards from './components/Cards/Cards.jsx'
-import Nav from "./components/Nav/Nav.jsx"
+import Nav from   "./components/Nav/Nav.jsx"
 import { useState } from 'react'
-import styled from './App.module.css'
+import { Route, Routes } from 'react-router-dom'
 
 function App () {
   const[characters, setCharacters] = useState([]);
@@ -10,9 +10,13 @@ function App () {
     const URL_BASE= "https://be-a-rym.up.railway.app/api";
     const KEY = "77a44721408a.840716731ca74f902c7b";
 
+    if (characters.find((char) => char.id === id)){
+      return alert ("Personaje Repetido");
+    }
+
     fetch(`${URL_BASE}/character/${id}?key=${KEY}`)
-    .then(response => response.json())
-    .then((data) =>{
+    .then((response) => response.json())
+    .then((data) => {
       if(data.name) {
         setCharacters((oldChars) => [...oldChars, data]);
       } else {
@@ -26,13 +30,14 @@ function App () {
   };
 
   return (
-    <div className='App' style={{ padding: '25px' }}>
-      <div className = {styled.nav} >
+    <div>
         <Nav onSearch = {onSearch} />
-      </div>
-      <div>
-        <Cards characters={characters} onClose={onClose} />
-      </div>
+        <Routes>
+            <Route
+                path='/home'
+                element={<Cards characters={characters} onClose={onClose} />}                
+              />
+        </Routes>
     </div>
   );
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import SearchBar from "../SearchBar/SearchBar"
+import { Link } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
 
 class Nav extends React.Component {
     constructor(props){
@@ -9,9 +10,10 @@ class Nav extends React.Component {
 render() {
     return(
         <div >
-
             <SearchBar onSearch={this.props.onSearch} />
-
+            <Link to= "/about" >
+             <h3>About</h3>
+             </Link>
         </div>
     );
 }
