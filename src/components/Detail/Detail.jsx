94ca85afd0638abd321fa38173519e1334fddf8a
CesarGomezPeloso <cesargomezpeloso@outlook.com>
2023-03-25 14:16:02 -0300

    React-Detail

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,19 +1,35 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const Detail = () => {
-    const params = useParams();
+    const {detailId} = useParams();
+    const [character, setCharacter]= useState({});
 
-
-return(
-    <div>
-        <h2>Nombre</h2>
-        <p>Status</p>
-        <p>Specie</p>
-        <p>Gender</p>
-        <p>Origin</p>
-        <p>Imagen</p>
-    </div>
-    );
-};
-
-export default Detail;
+    useEffect(() => {
+     const URL_BASE= "https://be-a-rym.up.railway.app/api";
+     const KEY = "77a44721408a.840716731ca74f902c7b";
+     axios(`${URL_BASE}/character/${detailId}?key=${KEY}`).then ((response) =>
+     setCharacter(response.data)
+     );
+    },[]);
+     
+     return (
+        <div>
+          {character.name ? (
+            <>
+              <h2>{character.name}</h2>
+              <p>{character.status}</p>
+              <p>{character.species}</p>
+              <p>{character.gender}</p>
+              <p>{character.origin?.name}</p>
+              <img src={character.image} alt="img" />
+            </>
+          ) : (
+            <h3>Loading...</h3>
+          )}
+        </div>
+      );
+    };
+    
+    export default Detail;
